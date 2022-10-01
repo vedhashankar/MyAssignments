24ad66dbf1b4d6bf63e16c3517b6ba9578a5ba3c
vedhashankar <114828150+vedhashankar@users.noreply.github.com>
2022-10-02 00:01:44 +0530

    Add files via upload
    day1

---
+++ day1/FirstProgram.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method s
+		int s = 5;
+		byte single = 123;
+		short num2 = 4566;
+		long simple = 99878;
+		float digit = (float) 57657.77;
+		double dict2 = 6546564.6565;
+		System.out.println("Hello world");
+		System.out.println("Hi digit");
+		
+
+	}
+
+}
